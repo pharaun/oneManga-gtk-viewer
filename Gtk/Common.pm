8bc4230856ce81eb0c9c68286154f48c1addbf7d
Anja Berens <pharaun666@gmail.com>
2008-11-24 10:12:57 -0700

    Committing what I got on the SpinButton for now, because soon going away for a bit

--- Gtk/Common.pm
+++ Gtk/Common.pm
@@ -10,10 +10,11 @@ our $VERSION = '0.03';
 use constant TRUE   => 1;
 use constant FALSE  => 0;
 
-use constant HPADDING => 12;
-use constant HPADDING_INDENT => 24;
+use constant HPADDING	=> 12;
+use constant INDENT	=> 12;
 
-use constant VPADDING => 6;
+use constant VPADDING	    => 6;
+use constant LABEL_VPADDING => 18;
 
 ###############################################################################
 # Static Final Global Vars
@@ -108,53 +109,58 @@ sub preferences_cb {
     # Tabbed notebook for all of the preferences categories
     my $notebook = Gtk2::Notebook->new();
     $notebook->set_show_tabs(TRUE);
-    my $notebook_align = Gtk2::Alignment->new(0, 0.5, 0, 0);
-    $notebook_align->set_padding(VPADDING, VPADDING, HPADDING, HPADDING);
-    $notebook_align->add($notebook);
-    $dialog->vbox()->pack_start($notebook_align, TRUE, TRUE, 0);
+    $dialog->vbox()->pack_start(_preference_border_padding($notebook), 
+	    TRUE, TRUE, 0);
 
     
     # Notebook page for General Settings
-    my $general = Gtk2::Table->new(3, 3, FALSE);
-    $notebook->append_page($general, 'General');
+    my $general = Gtk2::Table->new(4, 3, FALSE);
+    $notebook->append_page(_preference_border_padding($general), 
+	    'General');
 
     # Cache Label
-    my $cache_label = Gtk2::Label->new();
-    $cache_label->set_markup('<b>Cache</b>');
-    my $cache_label_align = Gtk2::Alignment->new(0, 0.5, 0, 0);
-    $cache_label_align->set_padding(0, 0, HPADDING, 0);
-    $cache_label_align->add($cache_label);
-    $general->attach($cache_label_align, 1, 4, 1, 2, 'fill', 'fill', 2, 2);
+    $general->attach(_preference_group_label('Cache'), 
+	    1, 4, 1, 2, 'fill', 'fill', 2, 2);
 
     # Cache Dir
-    my $cache_dir_label = Gtk2::Alignment->new(0, 0.5, 0, 0);
-    $cache_dir_label->set_padding(0, 0, HPADDING_INDENT, 0);
-    $cache_dir_label->add(Gtk2::Label->new('Cache Dir:'));
+    my $cache_dir_label = _preference_indent(Gtk2::Label->new('Cache Dir:'));
     my $cache_dir_entry = Gtk2::Entry->new();
-    my $cache_dir_button = Gtk2::Alignment->new(0, 0.5, 0, 0);
-    $cache_dir_button->set_padding(0, 0, 0, HPADDING_INDENT);
-    $cache_dir_button->add(Gtk2::Button->new('_Browse...'));
+    my $cache_dir_button = Gtk2::Button->new('_Browse...');
 
     $general->attach($cache_dir_label, 1, 2, 2, 3, 'fill', 'fill', 2, 2);
     $general->attach($cache_dir_entry, 2, 3, 2, 3, ['fill', 'expand'], 
 	    'fill', 2, 2);
     $general->attach($cache_dir_button, 3, 4, 2, 3, 'fill', 'fill', 2, 2);
+    
+
+    # Fetcher Label
+    $general->attach(_preference_group_label('Fetcher', TRUE), 
+	    1, 4, 3, 4, 'fill', 'fill', 2, 2);
+    
+    # Fetcher Delay 
+    my $fetcher_delay_label = _preference_indent(Gtk2::Label->new(
+		'Fetcher Delay:'));
+    my $fetcher_delay_spin = Gtk2::SpinButton->new_with_range(0, 10, 1);
+
+    $general->attach($fetcher_delay_label, 1, 2, 4, 5, 'fill', 'fill', 2, 2);
+    $general->attach($fetcher_delay_spin, 2, 4, 4, 5, ['fill', 'expand'], 
+	    'fill', 2, 2);
+
 
 
 
     # Notebook page for Manga List
-    my $list = Gtk2::VBox->new();
-    $notebook->append_page($list, 'Manga List');
+    my $list = Gtk2::Table->new(3, 3, FALSE);
+    $notebook->append_page(_preference_border_padding($list), 
+	    'Manga List');
     
-    $list->add(Gtk2::Label->new('da'));
 
 
 
     # Notebook page for Manga Viewer
-    my $viewer = Gtk2::VBox->new();
-    $notebook->append_page($viewer, 'Viewer');
-
-    $viewer->add(Gtk2::Label->new('da'));
+    my $viewer = Gtk2::Table->new(3, 3, FALSE);
+    $notebook->append_page(_preference_border_padding($viewer), 
+	    'Viewer');
 
 
 
@@ -197,6 +203,54 @@ sub init_menu_bar {
     #$window->add_accel_group ($ui->get_accel_group);
 
     return $ui->get_widget("/MenuBar");
+}
+
+
+###############################################################################
+# Initalizes the Preference Border Padding
+###############################################################################
+sub _preference_border_padding {
+    my $widget = shift;
+
+    my $align = Gtk2::Alignment->new(0, 0, 1, 1);
+    $align->set_padding(VPADDING, VPADDING, HPADDING, HPADDING);
+    $align->add($widget);
+
+    return $align;
+}
+
+
+###############################################################################
+# Initalizes the Preference 'Group' Label
+###############################################################################
+sub _preference_group_label {
+    my $label = shift;
+    my $not_first = shift;
+
+    my $label_widget = Gtk2::Label->new();
+    $label_widget->set_markup("<b>$label</b>");
+    my $label_align = Gtk2::Alignment->new(0, 0.5, 0, 0);
+    
+    if (defined $not_first) {
+	$label_align->set_padding(LABEL_VPADDING, 0, 0, 0);
+    }
+    $label_align->add($label_widget);
+
+    return $label_align;
+}
+
+
+###############################################################################
+# Initalizes the Preference Indent
+###############################################################################
+sub _preference_indent {
+    my $widget = shift;
+    
+    my $align = Gtk2::Alignment->new(0, 0.5, 0, 0);
+    $align->set_padding(0, 0, INDENT, 0);
+    $align->add($widget);
+
+    return $align;
 }
 
 1;
