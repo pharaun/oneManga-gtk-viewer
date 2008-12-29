3bcd33ea59e96b4f4f42f4f5c9871e9a1ed68c0b
Anja Berens <pharaun666@gmail.com>
2008-12-28 20:49:41 -0700

    Basic preference initalizer functionality working
    TODO:
     * Provide a way to load/save config values
     * Provide bit more flexability regarding customizability
     * Restructure the framework to lay out the widgets a bit better
     * Fix some glitches that are bound to appear

--- View/Preferences.pm
+++ View/Preferences.pm
@@ -3,6 +3,9 @@ use warnings;
 use strict;
 
 use Gtk2 '-init';
+use Data::Dumper;
+
+use View::Common;
 
 our $VERSION = '0.01';
 
@@ -24,140 +27,221 @@ use constant SCALE_TYPE => 'scale_type';
 # Static Final Global Vars
 ###############################################################################
 my $CONFIG = Util::Config->new();
-my @SCALE_TYPE = ('nearest', 'tiles', 'bilinear', 'hyper');
+
 
 
 ###############################################################################
 # Constructor
 ###############################################################################
 sub new {
-    my ($class, $window) = @_;
+    my ($class) = @_;
 
     my $self = {
-	_window	=> $window,
 	_ui	=> undef
     };
     bless $self, $class;
+    $self->_initalize();
+
     return $self;
 }
 
 
+# All of the tabs
+# Type of widgets: ComboBox, File Selector, SpinButton
+
+# Type: plabel - preference label,
+#	entry  - preference entry/widget
+
+my @PREFERENCE_ITEM = (
+ #  tab label
+ [  "General",
+    #	type,		widget type,    label,			    Widget specific,    Widget specific Args
+    [	"entry",	"dirbrowser",	"Cache Directory:",	    undef,		undef ],
+    [	"entry",	"spinbutton",	"Fetcher Delay:",	    \&_spinbutton,		[0, 10, 1] ]],
+ [  "Manga List",
+    []],
+ [  "Manga Viewer",
+    #	type,		widget type,    label,			    Widget specific,    Widget specific Args
+    [	"plabel",	undef,		"Scaling",		    undef,		undef ],
+    [	"entry",	"combobox",	"Scaling:",		    \&_combobox_text,   ['nearest', 'tiles', 'bilinear', 'hyper' ]],
+    [	"plabel",	undef,		"Best Fit",		    undef,		undef ],
+    [	"entry",	"radiobutton",	"Best Fit the page to:",    undef,		['Height', 'Width'] ]]
+);
+
+
 ###############################################################################
-# TODO: Not completed
-# Initalizes the preferences Dialog
+# Private Initalizer
 ###############################################################################
-sub _preferences_cb {
-    my ($callback_data, $callback_action, $widget) = @_;
-    
-    my $dialog = Gtk2::Dialog->new('oneManga Viewer Preferences',
+sub _initalize {
+    my ($self) = @_;
+  
+    # Create the Dialog Box
+    $self->{_ui} = Gtk2::Dialog->new('oneManga Viewer Preferences',
 	    undef,
 	    'destroy-with-parent',
 	    'gtk-cancel'    =>	'cancel',
 	    'gtk-ok'	    =>	'ok');
 
-    $dialog->set_default_response('ok');
-    $dialog->set_has_separator(FALSE);
+    $self->{_ui}->set_default_response('ok');
+    $self->{_ui}->set_has_separator(FALSE);
 
-    # Tabbed notebook for all of the preferences categories
+    # Create the notebook that holds all of the tabs for each preferences
     my $notebook = Gtk2::Notebook->new();
     $notebook->set_show_tabs(TRUE);
-    $dialog->vbox()->pack_start(border_padding($notebook), 
+    $self->{_ui}->vbox()->pack_start(View::Common::border_padding($notebook), 
 	    TRUE, TRUE, 0);
 
     
-    # Notebook page for General Settings
-    my $general = Gtk2::Table->new(4, 3, FALSE);
-    $notebook->append_page(border_padding($general), 
-	    'General');
+    foreach (@PREFERENCE_ITEM) {
+	my $tab_label = shift @$_;
+	my @tab_content = @$_;
 
-    # Cache Label
-    $general->attach(_preference_group_label('Cache'), 
-	    1, 4, 1, 2, 'fill', 'fill', 2, 2);
-
-    # Cache Dir
-    my $cache_dir_label = left_indent(Gtk2::Label->new('Cache Dir:'));
-    my $cache_dir_entry = Gtk2::Entry->new();
-    my $cache_dir_button = Gtk2::Button->new('_Browse...');
-
-    $general->attach($cache_dir_label, 1, 2, 2, 3, 'fill', 'fill', 2, 2);
-    $general->attach($cache_dir_entry, 2, 3, 2, 3, ['fill', 'expand'], 
-	    'fill', 2, 2);
-    $general->attach($cache_dir_button, 3, 4, 2, 3, 'fill', 'fill', 2, 2);
-    
-
-    # Fetcher Label
-    $general->attach(_preference_group_label('Fetcher', TRUE), 
-	    1, 4, 3, 4, 'fill', 'fill', 2, 2);
-    
-    # Fetcher Delay 
-    my $fetcher_delay_label = left_indent(Gtk2::Label->new(
-		'Fetcher Delay:'));
-    my $fetcher_delay_spin = Gtk2::SpinButton->new_with_range(0, 10, 1);
-
-    $general->attach($fetcher_delay_label, 1, 2, 4, 5, 'fill', 'fill', 2, 2);
-    $general->attach($fetcher_delay_spin, 2, 4, 4, 5, ['fill', 'expand'], 
-	    'fill', 2, 2);
+	# Generating the Tab Page
+	my $tab = Gtk2::Table->new($#tab_content, 3, FALSE);
+	$notebook->append_page(View::Common::border_padding($tab), $tab_label);
 
 
+	# Generating the contents of the Tab Page
+	my $i = 1; 
+	foreach (@tab_content) {
+	    my $type = shift @$_;
+	    my $widget_type = shift @$_;
+	    my $label = shift @$_;
+	    my @widget_specific = @$_;
+
+	    if (defined $type) {
+		if ($type eq 'plabel') {
+		    # Create a new preference label
+		    $tab->attach(_preference_group_label($label),
+			    1, 4, $i, ($i + 1), 'fill', 'fill', 2, 2);
+
+		} elsif ($type eq 'entry') {
+		    if (defined $widget_type) {
+			if ($widget_type eq 'combobox') {
+			    # Create a new Combo Box entry
+			    $tab->attach(View::Common::left_indent(
+					Gtk2::Label->new($label)),
+				    1, 2, $i, ($i + 1), 'fill', 'fill', 2, 2);
+
+			    $tab->attach(
+				    $widget_specific[0]->($widget_specific[1]),
+				    2, 4, $i, ($i + 1), ['fill', 'expand'],
+				    'fill', 2, 2);
+			   
+
+			} elsif ($widget_type eq 'radiobutton') {
+			    # Create a new Radio Button
+			    $tab->attach(View::Common::left_indent(
+					Gtk2::Label->new($label)),
+				    1, 4, $i, ($i + 1), 'fill', 'fill', 2, 2);
+			    
+			    my $prev = undef;
+			    foreach (@{$widget_specific[1]}) {
+				$prev = Gtk2::RadioButton->new($prev, $_);
+				$i++;
+				
+				$tab->attach(View::Common::left_indent(
+					View::Common::left_indent(
+					$prev)),
+				    1, 4, $i, ($i + 1), 'fill', 'fill', 2, 2);
+			    }
 
 
-    # Notebook page for Manga List
-    my $list = Gtk2::Table->new(3, 3, FALSE);
-    $notebook->append_page(border_padding($list), 
-	    'Manga List');
-    
+			} elsif ($widget_type eq 'spinbutton') {
+			    # Create a new Spinner widget
+			    $tab->attach(View::Common::left_indent(
+					Gtk2::Label->new($label)),
+				    1, 2, $i, ($i + 1), 'fill', 'fill', 2, 2);
+			    
+			    $tab->attach(
+				    $widget_specific[0]->($widget_specific[1]),
+				    2, 4, $i, ($i + 1), ['fill', 'expand'],
+				    'fill', 2, 2);
+			
+			
+			} elsif ($widget_type eq 'dirbrowser') {
+			    # Create a new directory browser
+			    $tab->attach(View::Common::left_indent(
+					Gtk2::Label->new($label)),
+				    1, 2, $i, ($i + 1), 'fill', 'fill', 2, 2);
 
-
-
-    # Notebook page for Manga Viewer
-    my $viewer = Gtk2::Table->new(3, 3, FALSE);
-    $notebook->append_page(border_padding($viewer), 
-	    'Viewer');
-
-    # Scaling Label
-    $viewer->attach(_preference_group_label('Scaling'), 
-	    1, 4, 1, 2, 'fill', 'fill', 2, 2);
-
-    # Scaling Type
-    my $scale_type_label = left_indent(Gtk2::Label->new(
-		'Scaling Type:'));
-    my $scale_type_dropdown = Gtk2::ComboBox->new_text();
-    foreach (@SCALE_TYPE) {
-	$scale_type_dropdown->append_text($_);
-    }
-    my $param = $CONFIG->get_param(BLOCK, SCALE_TYPE);
-    foreach (0 .. $#SCALE_TYPE) {
-	if ($param eq $SCALE_TYPE[$_]) {
-	    $scale_type_dropdown->set_active($_);
-	    last;
+			    $tab->attach(Gtk2::Entry->new(),
+				    2, 3, $i, ($i + 1), ['fill', 'expand'], 
+				    'fill', 2, 2);
+			    
+			    $tab->attach(Gtk2::Button->new('_Browse...'),
+				    3, 4, $i, ($i + 1), 'fill', 'fill', 2, 2);
+			}
+		    }
+		}
+	    }
+	    $i++;
 	}
     }
-
-    $viewer->attach($scale_type_label, 1, 2, 2, 3, 'fill', 'fill', 2, 2);
-    $viewer->attach($scale_type_dropdown, 2, 4, 2, 3, ['fill', 'expand'], 
-	    'fill', 2, 2);
-    
-
-    # Best Fit Label
-    $viewer->attach(_preference_group_label('Best Fit', TRUE), 
-	    1, 4, 3, 4, 'fill', 'fill', 2, 2);
     
 
 
 
-    
+
+
+
+
+#    my $param = $CONFIG->get_param(BLOCK, SCALE_TYPE);
+#    foreach (0 .. $#SCALE_TYPE) {
+#	if ($param eq $SCALE_TYPE[$_]) {
+#	    $scale_type_dropdown->set_active($_);
+#	    last;
+#	}
+#    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     # show and interact modally -- blocks until the user
     # activates a response.
-    $dialog->show_all();
-    my $response = $dialog->run();
+    $self->{_ui}->show_all();
+    my $response = $self->{_ui}->run();
     if ($response eq 'ok') {
-	$CONFIG->set_param(BLOCK, SCALE_TYPE, $scale_type_dropdown->get_active_text());
-	$CONFIG->save();
     }
 
     # activating a response does not destroy the window,
     # that's up to you.
-    $dialog->destroy();
+    $self->{_ui}->destroy();
+}
+
+
+
+###############################################################################
+# Private Combobox generator
+###############################################################################
+sub _combobox_text {
+    my @content = @{$_[0]};
+
+    my $combo_box = Gtk2::ComboBox->new_text();
+    foreach (@content) {
+	$combo_box->append_text($_);
+    }
+
+    return $combo_box;
+}
+
+
+###############################################################################
+# Private spinbutton generator
+###############################################################################
+sub _spinbutton {
+    my ($min, $max, $rate) = @{$_[0]};
+    
+    return Gtk2::SpinButton->new_with_range($min, $max, $rate);
 }
 
 
@@ -178,32 +262,6 @@ sub _preference_group_label {
     $label_align->add($label_widget);
 
     return $label_align;
-}
-
-
-###############################################################################
-# Check if a reference passed to it is indeed an array reference
-###############################################################################
-sub _is_array {
-    my ($ref) = @_;
-    # Firstly arrays need to be references, throw
-    #  out non-references early.
-    return FALSE unless ref $ref;
-
-    # Now try and eval a bit of code to treat the
-    #  reference as an array.  If it complains
-    #  in the 'Not an ARRAY reference' then we're
-    #  sure it's not an array, otherwise it was.
-    eval {
-	my $a = @$ref;
-    };
-    if ($@=~/^Not an ARRAY reference/) {
-	return FALSE;
-    } elsif ($@) {
-	die "Unexpected error in eval: $@\n";
-    } else {
-	return TRUE;
-    }
 }
 
 
