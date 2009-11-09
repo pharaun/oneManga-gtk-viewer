18a67fbd1d11c7885f18c2ee3c100a0b12aa79c2
Anja Berens <pharaun666@gmail.com>
2009-11-08 22:16:13 -0500

    Done with indexing & pagation
    TODO:
        * Fix it for chapters and pages only mangas
        * Extract the stupid if/elsif/else/end tree on back/forward sensitiveness

--- ruby-onemanga/manga_viewer.rb
+++ ruby-onemanga/manga_viewer.rb
@@ -244,31 +244,56 @@ def build_manga_viewer(dummy)
 	# Ignore changes that are from "forward/backward"
 	# setting the combobox active index
 	if combobox.active != dummy_pages.index
-	    puts "valid"
+	    puts "valid - page"
 	    image.pixbuf = dummy_pages.goto(combobox.active)
 
-	    # TODO: Extract this two if block into a function
+	    # Setup the forward/backward button status
+	    # TODO: extract to a function
 	    if dummy_pages.next?
 		forward.sensitive = true
 	    else
-		if dummy_chapters.nil?
+		if dummy_chapters.nil? and dummy_volumes.nil?
 		    forward.sensitive = false
-		elsif dummy_chapters.next?
-		    forward.sensitive = true
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			forward.sensitive = false
+		    end
 		else
-		    forward.sensitive = false
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			if dummy_volumes.next?
+			    forward.sensitive = true
+			else
+			    forward.sensitive = false
+			end
+		    end
 		end
 	    end
 
 	    if dummy_pages.prev?
 		back.sensitive = true
 	    else
-		if dummy_chapters.nil?
+		if dummy_chapters.nil? and dummy_volumes.nil?
 		    back.sensitive = false
-		elsif dummy_chapters.prev?
-		    back.sensitive = true
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			back.sensitive = false
+		    end
 		else
-		    back.sensitive = false
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			if dummy_volumes.prev?
+			    back.sensitive = true
+			else
+			    back.sensitive = false
+			end
+		    end
 		end
 	    end
 	end
@@ -281,39 +306,72 @@ def build_manga_viewer(dummy)
     chapters.signal_connect('changed') do |combobox|
 	# Ignore changes that are from "forward/backward"
 	# setting the combobox active index
-	if ((combobox.active != dummy.chapters.index(dummy_chapters)) unless dummy.chapters.nil?)
-	    # TODO: Fix above if statement
-	    puts "valid"
+	if combobox.active != dummy_volumes.chapters.index(dummy_chapters)
+	    puts "valid - chapter"
 
-	    dummy_chapters = dummy.chapters[combobox.active]
+	    dummy_chapters = dummy_volumes.chapters[combobox.active]
 	    dummy_pages = dummy_chapters.pages
 
 	    image.pixbuf = dummy_pages.first
 
+	    # Update the combo box listing for pages
+	    # Update the pages combobox
+	    list_store = Gtk::ListStore.new(String)
+	    dummy_pages.page_titles.each do |text|
+		(list_store.append())[0] = text
+	    end
+	    pages.model = list_store
+
+	    # Set the combo box index
 	    pages.active = 0
 
-	    # TODO: Extract this two if block into a function
+	    # Setup the forward/backward button status
+	    # TODO: extract to a function
 	    if dummy_pages.next?
 		forward.sensitive = true
 	    else
-		if dummy_chapters.nil?
+		if dummy_chapters.nil? and dummy_volumes.nil?
 		    forward.sensitive = false
-		elsif dummy_chapters.next?
-		    forward.sensitive = true
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			forward.sensitive = false
+		    end
 		else
-		    forward.sensitive = false
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			if dummy_volumes.next?
+			    forward.sensitive = true
+			else
+			    forward.sensitive = false
+			end
+		    end
 		end
 	    end
 
 	    if dummy_pages.prev?
 		back.sensitive = true
 	    else
-		if dummy_chapters.nil?
+		if dummy_chapters.nil? and dummy_volumes.nil?
 		    back.sensitive = false
-		elsif dummy_chapters.prev?
-		    back.sensitive = true
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			back.sensitive = false
+		    end
 		else
-		    back.sensitive = false
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			if dummy_volumes.prev?
+			    back.sensitive = true
+			else
+			    back.sensitive = false
+			end
+		    end
 		end
 	    end
 	end
@@ -326,7 +384,83 @@ def build_manga_viewer(dummy)
     volumes.signal_connect('changed') do |combobox|
 	# Ignore changes that are from "forward/backward"
 	# setting the combobox active index
-	if (true)
+	if combobox.active != dummy.volumes.index(dummy_volumes)
+	    puts "valid - volume"
+
+	    dummy_volumes = dummy.volumes[combobox.active]
+	    dummy_chapters = dummy_volumes.chapters[0] # assuming arrays
+	    dummy_pages = dummy_chapters.pages
+
+	    image.pixbuf = dummy_pages.first
+
+	    # Update the combo box listing for chapters & pages
+	    # Update the pages combobox
+	    list_store = Gtk::ListStore.new(String)
+	    dummy_pages.page_titles.each do |text|
+		(list_store.append())[0] = text
+	    end
+	    pages.model = list_store
+
+	    # Update the chapters combobox
+	    list_store = Gtk::ListStore.new(String)
+	    dummy_volumes.chapters.each do |chp|
+		(list_store.append())[0] = chp.title
+	    end
+	    chapters.model = list_store
+
+	    # Set the combo box index
+	    chapters.active = 0
+	    pages.active = 0
+
+	    # Setup the forward/backward button status
+	    # TODO: extract to a function
+	    if dummy_pages.next?
+		forward.sensitive = true
+	    else
+		if dummy_chapters.nil? and dummy_volumes.nil?
+		    forward.sensitive = false
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			forward.sensitive = false
+		    end
+		else
+		    if dummy_chapters.next?
+			forward.sensitive = true
+		    else
+			if dummy_volumes.next?
+			    forward.sensitive = true
+			else
+			    forward.sensitive = false
+			end
+		    end
+		end
+	    end
+
+	    if dummy_pages.prev?
+		back.sensitive = true
+	    else
+		if dummy_chapters.nil? and dummy_volumes.nil?
+		    back.sensitive = false
+		elsif dummy_volumes.nil?
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			back.sensitive = false
+		    end
+		else
+		    if dummy_chapters.prev?
+			back.sensitive = true
+		    else
+			if dummy_volumes.prev?
+			    back.sensitive = true
+			else
+			    back.sensitive = false
+			end
+		    end
+		end
+	    end
 	end
     end
 
@@ -336,167 +470,4 @@ def build_manga_viewer(dummy)
     # Show/hide widgets
     #builder.get_object('chapters_hbox').hide_all
     #builder.get_object('volumes_hbox').hide_all
-end
-
-
-
-def tmp
-    # Load first image
-    if dummy.next_page?
-	image.pixbuf = dummy.first_page
-
-	# Set the buttons status
-	back.sensitive = false
-
-	# Setup the pages combo_box
-	list_store = Gtk::ListStore.new(String)
-	dummy.list_pages.each do |text|
-	    (list_store.append())[0] = text
-	end
-
-	render = Gtk::CellRendererText.new
-	pages.pack_start(render, true)
-	pages.set_attributes(render, {"text" => 0})
-
-	pages.model = list_store
-	pages.active = 0
-
-	# Setup the chapters combo_box
-	list_store = Gtk::ListStore.new(String)
-	dummy.list_chapters.each do |text|
-	    (list_store.append())[0] = text
-	end
-
-	render = Gtk::CellRendererText.new
-	chapters.pack_start(render, true)
-	chapters.set_attributes(render, {"text" => 0})
-
-	chapters.model = list_store
-	chapters.active = 0
-
-	# Setup the volumes combo_box
-	list_store = Gtk::ListStore.new(String)
-	dummy.list_volumes.each do |text|
-	    (list_store.append())[0] = text
-	end
-
-	render = Gtk::CellRendererText.new
-	volumes.pack_start(render, true)
-	volumes.set_attributes(render, {"text" => 0})
-
-	volumes.model = list_store
-	volumes.active = 0
-    end
-
-    # Connect the forward/back button Signals
-    forward.signal_connect('clicked') do
-	if dummy.next_page?
-	    puts "Next page"
-	    image.pixbuf = dummy.next_page
-
-	    pages.active = dummy.currentPageIndex
-	    back.sensitive = true
-	elsif dummy.next_chapter?
-	    puts "Next chapter"
-	    dummy = dummy.next_chapter
-
-	    # Loading first page of the new chapter
-	    puts "Next chapter - first page"
-	    image.pixbuf = dummy.first_page
-
-	    pages.active = dummy.currentPageIndex
-	    chapters.active = dummy.currentChapterIndex
-	    back.sensitive = true
-	elsif dummy.next_volume?
-	    puts "Next volume"
-	    dummy = dummy.next_volume
-
-	    # Loading first page of the new volume/chapter
-	    puts "Next volume - first page"
-	    image.pixbuf = dummy.first_page
-
-	    pages.active = dummy.currentPageIndex
-	    chapters.active = dummy.currentChapterIndex
-	    volumes.active = dummy.currentVolumeIndex
-	    back.sensitive = true
-	end
-
-	if !dummy.next_page? and !dummy.next_chapter? and !dummy.next_volume?
-	    forward.sensitive = false
-	end
-    end
-
-    back.signal_connect('clicked') do
-	if dummy.prev_page?
-	    puts "Prev page"
-	    image.pixbuf = dummy.prev_page
-
-	    pages.active = dummy.currentPageIndex
-	    forward.sensitive = true
-	elsif dummy.prev_chapter?
-	    puts "Prev chapter"
-	    dummy = dummy.prev_chapter
-
-	    # Loading first page of the new chapter
-	    puts "Prev chapter - Last page"
-	    image.pixbuf = dummy.last_page
-
-	    pages.active = dummy.currentPageIndex
-	    chapters.active = dummy.currentChapterIndex
-	    forward.sensitive = true
-	elsif dummy.prev_volume?
-	    puts "Prev volume"
-	    dummy = dummy.prev_volume
-
-	    # Loading first page of the new chapter
-	    puts "Prev volume - Last page"
-	    image.pixbuf = dummy.last_page
-
-	    pages.active = dummy.currentPageIndex
-	    chapters.active = dummy.currentChapterIndex
-	    volumes.active = dummy.currentVolumeIndex
-	    forward.sensitive = true
-	end
-
-	if !dummy.prev_page? and !dummy.prev_chapter? and !dummy.prev_volume?
-	    back.sensitive = false
-	end
-    end
-
-    chapters.signal_connect('changed') do |combobox|
-	# Ignore changes that are from "forward/backward"
-	# setting the combobox active index
-	if combobox.active != dummy.currentChapterIndex
-	    puts "valid"
-	    image.pixbuf = dummy.goto_chapter(combobox.active)
-
-	    # update the page index
-	    pages.active = dummy.currentPageIndex
-	end
-    end
-
-    volumes.signal_connect('changed') do |combobox|
-	# Ignore changes that are from "forward/backward"
-	# setting the combobox active index
-	if combobox.active != dummy.currentVolumeIndex
-	    puts "valid"
-	    image.pixbuf = dummy.goto_volume(combobox.active)
-
-	    # update the chapter index
-	    chapters.active = dummy.currentChapterIndex
-
-	    # update the page index
-	    pages.active = dummy.currentPageIndex
-	end
-    end
-
-    # Connect the combo box selection
-    pages.signal_connect('changed') do |combobox|
-	# Ignore changes that are from "forward/backward"
-	# setting the combobox active index
-	if combobox.active != dummy.currentPageIndex
-	    puts "valid"
-	    image.pixbuf = dummy.goto_page(combobox.active)
-	end
-    end
 end
